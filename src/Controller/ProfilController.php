0e4162a2a16a1c0116bb0602bec76c928de3a8fc
prudhomme victor <vic301202@gmail.com>
2021-12-08 21:27:14 +0100

    ajout de la secu

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -6,10 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class ProfilController extends AbstractController
 {
     /**
      * @Route("/profil", name="profil")
+     * 
      */
     public function index(): Response
     {
