f45edcb87d877f6ab551874b5aa2196b9c5c802f
prudhomme victor <vic301202@gmail.com>
2021-12-08 21:15:37 +0100

    maj to sf 5.4

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
